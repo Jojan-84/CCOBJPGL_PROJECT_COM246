23d66f6f372a6c7fd85c0078382804153193d6b6
edriantala <talaeb@students.national-u.edu.ph>
2025-05-30 02:17:45 +0800

    update README.md and refactor project structure

--- mcdonalds/src/main/java/module-info.java
+++ mcdonalds/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.example to javafx.fxml;
-    exports com.example;
+    opens mcdo to javafx.graphics;
+    opens mcdo.controller to javafx.fxml;
+
+    exports mcdo;
 }
